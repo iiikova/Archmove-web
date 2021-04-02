fd240614d4c47156f97fe23aa9e6ef459233e7d9
Ihor Koval <nicholakoval@gmail.com>
2021-04-02 12:13:38 +0300

    maket_final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HeaderComponent } from './components/header/header.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { FeatureComponent } from './components/feature/feature.component';
+import { RechtangleComponent } from './components/rechtangle/rechtangle.component';
+import { PartnersComponent } from './components/partners/partners.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeaderComponent,
+    GalleryComponent,
+    FeatureComponent,
+    RechtangleComponent,
+    PartnersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/rechtangle/rechtangle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rechtangle',
+  templateUrl: './rechtangle.component.html',
+  styleUrls: ['./rechtangle.component.scss']
+})
+export class RechtangleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
